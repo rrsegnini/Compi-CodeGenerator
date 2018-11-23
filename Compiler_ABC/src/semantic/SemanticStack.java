9f78dfc6417867ebba9762eb2dc033d61b329385
Daniel <dab18@hotmail.com>
2018-11-23 00:26:31 -0600

    Stack, just naming methods

--- Compiler_ABC/src/semantic/SemanticStack.java
+++ Compiler_ABC/src/semantic/SemanticStack.java
@@ -9,6 +9,36 @@ package semantic;
  *
  * @author danielalvarado
  */
-public class SemanticStack {
+public class SemanticStack<RS> implements Stack {
+
+    @Override
+    public boolean push(Object value) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Object pop() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Object search() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean contains() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void clear() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean isEmpty() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
