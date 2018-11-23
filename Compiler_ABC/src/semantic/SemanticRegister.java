9c19a0846d922be325f60efc3725815337d32f64
Daniel <dab18@hotmail.com>
2018-11-23 13:42:53 -0600

    Getters and setters SR

--- Compiler_ABC/src/semantic/SemanticRegister.java
+++ Compiler_ABC/src/semantic/SemanticRegister.java
@@ -12,6 +12,50 @@ package semantic;
 public class SemanticRegister {
     
     String descrp;
+    String valor;
+    String type;
+
+    /**
+     * Constructor used for WHILE IF 
+     * @param descrp SR name 
+     * @param valor  SR token being stored 
+     */
+    public SemanticRegister(String descrp, String valor) {
+        this.descrp = descrp;
+        this.valor = valor;
+    }
+       
+    /**
+     * Constructor used for SR_DO (constants and variables)
+     * @param descrp SR name
+     * @param valor  SR token being stored
+     * @param type   SR type of SR_DO
+     */
+    public SemanticRegister(String descrp, String valor, String type) {
+        this.descrp = descrp;
+        this.valor = valor;
+        this.type = type;
+    }
+
+    
+    
+    
+    public String getValor() {
+        return valor;
+    }
+
+    public void setValor(String valor) {
+        this.valor = valor;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+    
 
     public String getDescrp() {
         return descrp;
@@ -20,6 +64,7 @@ public class SemanticRegister {
     public void setDescrp(String descrp) {
         this.descrp = descrp;
     }
+    
     
     
 }
